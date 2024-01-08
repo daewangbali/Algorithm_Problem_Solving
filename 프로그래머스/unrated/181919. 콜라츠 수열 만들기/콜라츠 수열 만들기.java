38173d4d244bc2c416f38e5e31629a9fff2bf6cc
daewangbali <76726223+daewangbali@users.noreply.github.com>
2024-01-08 11:43:13 +0900

    [level unrated] Title: 콜라츠 수열 만들기, Time: 3.01 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+        List<Integer> list = new ArrayList<>();
+        while(true){
+            list.add(n);
+            if(n == 1) break;
+            
+            if(n % 2 == 0){
+                n /= 2;
+            }else{
+                n = (3 * n + 1);
+            }
+        }
+        
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
