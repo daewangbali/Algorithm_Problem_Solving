3f140925d2ebee3ce7eb37f694de78a548aaffa6
daewangbali <76726223+daewangbali@users.noreply.github.com>
2024-01-11 11:35:42 +0900

    [level 1] Title: K번째수, Time: 0.33 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int idx = 0 ; idx < commands.length ; idx++){
+            int i = commands[idx][0] < 1 ? 0 : commands[idx][0]-1;
+            int j = commands[idx][1] > array.length ? array.length : commands[idx][1];
+            int k = commands[idx][2];
+            
+            int[] tmp = Arrays.copyOfRange(array, i, j);
+            Arrays.sort(tmp);
+            answer[idx] = tmp[k-1];
+        }
+        
+        return answer;
+    }
+}
