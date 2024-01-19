dd0dd1bb4947f89a33cbb2f18da30f28f823238b
daewangbali <76726223+daewangbali@users.noreply.github.com>
2024-01-19 15:09:10 +0900

    [level 1] Title: 모의고사, Time: 2.93 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -4,24 +4,24 @@ class Solution {
         int[] p1 = {1, 2, 3, 4, 5};
         int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] correct = new int[3];
+        int[] score = new int[3];
         
         for(int i = 0 ; i < answers.length ; i++){
-            if(answers[i] == p1[i - ((i / p1.length) * p1.length)]) 
-                correct[0]++;
-            if(answers[i] == p2[i - ((i / p2.length) * p2.length)]) 
-                correct[1]++;
-            if(answers[i] == p3[i - ((i / p3.length) * p3.length)]) 
-                correct[2]++;
+            if(answers[i] == p1[i % p1.length]) 
+                score[0]++;
+            if(answers[i] == p2[i % p2.length]) 
+                score[1]++;
+            if(answers[i] == p3[i % p3.length]) 
+                score[2]++;
         }
         
-        int max = Math.max(Math.max(correct[0], correct[1]), correct[2]);
+        int max = Math.max(Math.max(score[0], score[1]), score[2]);
         List<Integer> list = new ArrayList<>();
-        for(int i=0 ; i<correct.length ; i++){
-            if(correct[i] == max)
+        for(int i=0 ; i<score.length ; i++){
+            if(score[i] == max)
                 list.add(i + 1);
         }
-        
+    
         return list.stream().mapToInt(i -> i).toArray();
     }
 }
