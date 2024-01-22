2bfbb24d314f53d38eeeea784a3864bbdae3ceeb
daewangbali <76726223+daewangbali@users.noreply.github.com>
2024-01-22 23:38:21 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,21 +1,17 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        List<Integer> list = new ArrayList<>();
         
-        for(int i=1 ; i<=Math.sqrt(yellow) ; i++){
+        for(int i=1 ; i <= Math.sqrt(yellow) ; i++){
             if((yellow / i) != 0){
-                list.add(i);
-            }
-        }
-        
-        for(int i=0 ; i<list.size() ; i++){
-            int w = yellow / list.get(i);
-            int h = list.get(i);
-            if(w * h == yellow && ( w + 2 ) * ( h + 2 ) == (brown + yellow)){
-                return new int[] {w+2 , h+2};
+                int w = yellow / i;
+                int h = i;
+                if(w * h == yellow && ( w + 2 ) * ( h + 2 ) == (brown + yellow)){
+                    //return new int[] {w + 2 , h + 2};
+                    answer[0] = w + 2;
+                    answer[1] = h + 2;
+                    break;
+                }
             }
         }
         
