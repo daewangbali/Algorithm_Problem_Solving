31996ce9560920e2311b43477abb83efc2ec658f
daewangbali <76726223+daewangbali@users.noreply.github.com>
2024-01-26 12:16:42 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.06 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(long n) {
+        long answer = -1;
+        long sqrtN = (long) Math.sqrt(n);
+        if(Math.pow(sqrtN, 2) == n)
+            answer = (long) Math.pow(sqrtN+1, 2);
+        
+        return answer;
+    }
+}
