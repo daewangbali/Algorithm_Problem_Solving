610ec2eda7768ed794208c59369a174067142c58
daewangbali <76726223+daewangbali@users.noreply.github.com>
2024-01-31 12:58:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.25 ms, Memory: 97.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,17 +1,17 @@
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
 
-        for (int i=0; i<completion.length; i++) {
-            if (participant[i].equals(completion[i])) {
-                continue;
-            }else {
-                return participant[i];
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
             }
         }
-
-    return participant[participant.length-1];
+        return answer;
     }
 }
